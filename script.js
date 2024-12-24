1538c7d7777424df13788a2cdb30d1ae26018034
tomasbennett <tjs.bennett@gmail.com>
2024-12-24 20:59:43 +0000

    Completing the foundation javascript and css necessary for the working Etch-a-sketc features that I wanted

--- script.js
+++ script.js
@@ -1,48 +1,123 @@
-const defaultColour = document.querySelector('.grid.settings .colour-select div');
-const penColour = document.querySelector('.colour.settings .colour-select div');
+const defaultColour = document.querySelector('.grid.settings .colour-select > div input');
+const penColour = document.querySelector('.colour.settings .colour-select > div input');
 const innerEtch = document.querySelector('.inner-etch');
 
-const randomColours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "white", "gray", "silver", "maroon", "fuchsia", "purple", "lime", "aqua", "teal", "navy", "olive", "brown", "pink", "beige", "gold", "coral", "turquoise", "peachpuff", "chartreuse", "plum"];
+defaultColour.addEventListener('input', () => document.documentElement.style.setProperty('--default-colour', defaultColour.value));
+penColour.addEventListener('input', () => document.documentElement.style.setProperty('--pen-colour', penColour.value));
 
 
+
+
+const eraser = document.querySelector('#eraser');
+const toggleGridLines = document.querySelector('#toggle-grid');
 const gridFormatText = document.querySelector('#slider-value');
 const horizontalSlider = document.querySelector('#horizontal-slider');
+const clear = document.querySelector('.clear.settings .inputs');
+const rainbowElem = document.querySelector('#rainbow-element');
+
+let gridSquares = Array.from(innerEtch.children);
+
+horizontalSlider.addEventListener('input', gridSizeChange);
+document.addEventListener('DOMContentLoaded', gridSizeChange);
+eraser.addEventListener('click', () => {
+
+    if (rainbowElem.classList.contains('rainbow-active')) {
+        rainbowElem.classList.remove('rainbow-active');
+
+    }
+    eraser.classList.toggle('eraser-active');
+
+});
+rainbowElem.addEventListener('click', () => {
+    if (eraser.classList.contains('eraser-active')) {
+        eraser.classList.remove('eraser-active');
+        
+    }
+    rainbowElem.classList.toggle('rainbow-active');
+
+});
+toggleGridLines.addEventListener('click', () => {
+    toggleGridLines.classList.toggle('toggle-grid');
+
+    if (toggleGridLines.classList.contains('toggle-grid')) {
+        document.documentElement.style.setProperty('--toggle-border', '.1rem');
+
+    } else {
+        document.documentElement.style.setProperty('--toggle-border', '0rem');
+    
+    }
+});
+clear.addEventListener('click', clearGrid)
 
 
-gridFormatText.textContent = `Grid: ${horizontalSlider.value} x ${horizontalSlider.value}`;
-modifyingGridSquares();
+let mouseDown = 0;
+document.body.onmousedown = function () {
+    ++mouseDown;
+};
+document.body.onmouseup = function () {
+    --mouseDown;
+};
 
-horizontalSlider.addEventListener('input', () => {
-    modifyingGridSquares();
-
-    gridFormatText.textContent = `Grid: ${horizontalSlider.value} x ${horizontalSlider.value}`;
+document.body.addEventListener('dragstart', (event) => {
+    event.preventDefault();
 });
 
 
-function modifyingGridSquares() {
+const randomColours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "gray", "silver", "maroon", "fuchsia", "purple", "lime", "aqua", "teal", "navy", "olive", "brown", "pink", "beige", "gold", "coral", "turquoise", "peachpuff", "chartreuse", "plum"];
 
-    let gridSquares = Array.from(innerEtch.children);
-    let gridSquareCount = gridSquares.length;
+function paint(e) {
+    if (eraser.classList.contains('eraser-active')) {
+        e.target.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--default-colour');
+        return;
+    } else if (rainbowElem.classList.contains('rainbow-active')) {
+        e.target.style.backgroundColor = randomColours[Math.floor(Math.random() * randomColours.length)];
+        return;
+    }
     
+    e.target.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--pen-colour');
+}
+
+
+function clearGrid() {
+    gridSquares.forEach((elem) => {
+        elem.style.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--default-colour');
+    })
+}
+
+function gridSizeChange() {
+    gridFormatText.textContent = `Grid: ${horizontalSlider.value} x ${horizontalSlider.value}`;
+    modifyingGridSquares();
+}
+
+function modifyingGridSquares() {
+    
+    let gridSize = horizontalSlider.value ** 2;
+
     innerEtch.style.gridTemplateRows = `repeat(${horizontalSlider.value}, 1fr)`;
     innerEtch.style.gridTemplateColumns = `repeat(${horizontalSlider.value}, 1fr)`;
 
-    if(gridSquareCount <= horizontalSlider.value ** 2) {
-
-        for(let i = 0; i < horizontalSlider.value ** 2; i++){
-            const newGridSquare = document.createElement('div');
-            
-            newGridSquare.classList.add('grid-square');
-            innerEtch.appendChild(newGridSquare);
-        }
-            
-    } else {
-
-        for(let i = gridSquareCount; i > horizontalSlider.value ** 2; i--){
-            innerEtch.removeChild(innerEtch.lastElementChild);
-        }
+    while (gridSquares.length < gridSize) {
+        const newGridSquare = document.createElement('div');
+        newGridSquare.classList.add('grid-square');
         
+        newGridSquare.addEventListener('mousedown', paint);
+        newGridSquare.addEventListener('mousemove', (e) => {
+            if (mouseDown) {
+                paint(e);
+            }
+        });
+        innerEtch.appendChild(newGridSquare);
+        
+        gridSquares.push(newGridSquare);
+
+    }
+
+    while (gridSquares.length > gridSize) {
+        innerEtch.removeChild(innerEtch.lastElementChild);
+
+        gridSquares.pop()
 
     }
 
 }
+
